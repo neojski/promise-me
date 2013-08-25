18a2964195d39f27666c2570291631e8c4cfea8e
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2013-08-25 16:09:59 +0200

    Add travis-ci

---
+++ test/adapter.js
@@ -0,0 +1,21 @@
+var deferred = require('../promiseme.js');
+module.exports = {
+  fulfilled: function(value){
+    var d = deferred();
+    d.fulfill(value);
+    return d.promise;
+  },
+  rejected: function(reason){
+    var d = deferred();
+    d.reject(reason);
+    return d.promise;
+  },
+  pending: function(){
+    var d = deferred();
+    return {
+      promise: d.promise,
+      fulfill: d.fulfill,
+      reject: d.reject
+    }
+  }
+};
